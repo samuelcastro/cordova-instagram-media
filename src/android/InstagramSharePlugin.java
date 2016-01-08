62ba4b7a9cb139407938bdbc254ca765ab6de5a9
Samuel Castro <samuelcastrosilva@gmail.com>
2016-01-08 09:57:53 -0200

    Adding some comments

--- src/android/InstagramSharePlugin.java
+++ src/android/InstagramSharePlugin.java
@@ -1,6 +1,6 @@
 /*
     The MIT License (MIT)
-    Copyright (c) 2013 Vlad Stirbu
+    Copyright (c) 2016 Samuel Castro
 
     Permission is hereby granted, free of charge, to any person obtaining
     a copy of this software and associated documentation files (the
